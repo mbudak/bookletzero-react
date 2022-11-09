8156395a70ded71f80530613bbc324ded9ef1b99
mbudak <promaxcoq@gmail.com>
2022-11-08 21:45:15 -0500

    hero section updated

--- app/components/front/Hero.tsx
+++ app/components/front/Hero.tsx
@@ -1,5 +1,6 @@
 import { Link } from "@remix-run/react";
 // import { useTranslation } from "react-i18next";
+import HeroImage from "~/assets/img/hero.webp";
 
 import Header from "./Header";
 
@@ -8,88 +9,32 @@ export default function Hero() {
   return (
     <div className="relative">
       <Header />
-      <div className="relative overflow-hidden">
-        <div className="hidden lg:block lg:absolute lg:inset-0" aria-hidden="true">
-          <svg
-            className="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
-            width="404"
-            height="784"
-            fill="none"
-            viewBox="0 0 404 784"
-          >
-            <defs>
-              <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" className="text-gray-200 dark:text-gray-800" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width="404" height="784" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-          </svg>
-          <svg
-            className="  absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2 text-gray-200 dark:text-gray-800"
-            width="404"
-            height="784"
-            fill="none"
-            viewBox="0 0 404 784"
-          >
-            <defs>
-              <pattern id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width="404" height="784" fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
-          </svg>
-        </div>
 
-        <div className="relative">
-          <main className="mt-8 mx-auto max-w-7xl px-4 sm:mt-16 sm:px-6 lg:mt-16">
-            <div>
-              <div className="sm:text-center md:max-w-6xl md:mx-auto lg:col-span-6 space-y-3">
-                <span className="block text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
-                  BookletZero is an exam booklet generator, easily.
-                </span>
-                <h1 className="font-extrabold text-5xl sm:text-5xl md:text-5xl lg:text-7xl flex flex-col">
-                  <span>Power your knowledge with pdf exam booklets</span>
-                </h1>
-                <div className="relative z-10 pb-10 text-gray-500 text-lg md:text-2xl sm:text-center leading-normal md:leading-9 space-y-3">
-                  {/* <p className="sm:text-2xl mx-auto max-w-4xl font-medium text-gray-700 dark:text-gray-400">{t("front.hero.headline2")}</p> */}
-                  <p className="sm:text-xl mx-auto max-w-4xl">
-                    Strengthen yourself to prepare the next exam. 
-                  </p>
-                </div>
-
-                <div className="mt-2 mx-auto lg:mx-0 sm:flex justify-center md:mt-2">
-                  <div className="rounded-md shadow">
-                    <a
-                      href="/register"
+      <section className="bg-white dark:bg-gray-900">
+    <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Power your knowledge with PDF exam booklets, easily</h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">BookletZero helps to generate exam booklets for students, teacher & content generators.</p>
+            
+            
+            <a href="/register"
                       target="_self"
-                      rel="noreferrer"
-                      className="w-full flex items-center space-x-2 justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md bg-theme-500 text-theme-50 hover:bg-theme-600 md:py-4 md:text-lg md:px-7"
-                    >
-                      <div>Try for Free</div>
-                    </a>
-                  </div>
-                  
+                      rel="noreferrer" className="mx-2 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-100 border border-red-500 rounded-lg hover:bg-red-600 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 bg-red-500">
+                Register for Free
+            </a> 
+            <a href="/learn"
+                      target="_self"
+                      rel="noreferrer" className="mx-2 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                Learn List
+            </a> 
 
-                  <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                    <Link
-                      to="/learn"
-                      className="w-full flex items-center justify-center px-5 py-3 border border-gray-200 dark:border-gray-800 text-base font-medium rounded-md text-theme-600 dark:text-theme-400 bg-white hover:bg-gray-50 dark:bg-gray-900 dark:hover:text-theme-500 md:py-4 md:text-lg md:px-7"
-                    >
-                      Learn List
-                    </Link>
-                  </div>
-                </div>
-
-                <div className="mt-8 space-y-3">
-                  
-                    Find your interested topic and generate exam booklets easily from the frequently updated question databases.
-                  
-                </div>
-              </div>
-            </div>
-          </main>
         </div>
-      </div>
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">            
+            <img src={HeroImage} className="h-fit" alt="BookletZero Hero Image"/>
+        </div>                
     </div>
+</section>
+</div>
+
   );
 }
