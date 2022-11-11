6a1b8c34432b4fa78b78019e09d0625fe7c8cdb0
mbudak <promaxcoq@gmail.com>
2022-11-11 15:19:17 -0500

    here slogan improved

--- app/components/front/Hero.tsx
+++ app/components/front/Hero.tsx
@@ -14,10 +14,10 @@ export default function Hero() {
     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Power your knowledge with PDF exam booklets, easily
+            BookletZero help to improve your exam performance with practice, easily.
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              BookletZero helps to generate exam booklets for students, teacher & content generators.
+              Generate personal unique exam booklets for students, teacher & content generators so bookletzero will tell you what subject you need to focus on.
             </p>
             
             
@@ -29,7 +29,7 @@ export default function Hero() {
             <a href="/learn"
                       target="_self"
                       rel="noreferrer" className="mx-2 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Learn List
+                View Exam Booklets
             </a> 
 
         </div>
--- app/components/front/Landing.tsx
+++ app/components/front/Landing.tsx
@@ -4,7 +4,9 @@ export default function Landing() {
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto ">
                 <div className="mr-auto place-self-center ">
                     <h1 className="max-w-auto mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl dark:text-white">Everything you need to prepare for exam is a PDF booklet</h1>
-                    <p className="max-w mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">BookletZero comes with great features that helps you better preparation for any exam</p>
+                    <p className="max-w mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                        BookletZero comes with great features that helps you better preparation for any exam
+                    </p>
 
                 </div>
             </div>
